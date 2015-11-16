2ab403ca248d0b9ddcabbce8f99aa3fc8284918f
Mahdi Dibaiee <mdibaiee@aol.com>
2015-11-16 09:00:44 +0330

    feat(options): accept actual emojis as options feat(result): issue `result(s)` command to view the results fix(reactions): should only accept reactions if survey is active perf(listeners): listen on reaction events once, run a loop inside fix(response): don't respond to commands unless arguments are provided

--- build/index.js
+++ build/index.js
@@ -9,13 +9,7 @@ var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = 
 exports['default'] = function (bot) {
   var surveys = [];
 
-  bot.listen(/survey close (\d+)/i, function (message) {
-    var _message$match = _slicedToArray(message.match, 2);
-
-    var id = _message$match[1];
-
-    surveys[id].active = false;
-
+  var formatResult = function formatResult(survey) {
     var result = '';
 
     var _iteratorNormalCompletion = true;
@@ -23,10 +17,10 @@ exports['default'] = function (bot) {
     var _iteratorError = undefined;
 
     try {
-      for (var _iterator = Object.keys(surveys[id].votes)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      for (var _iterator = Object.keys(survey.votes)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
         var vote = _step.value;
 
-        var value = surveys[id].votes[vote];
+        var value = survey.votes[vote];
         result += ':' + vote + ': => ' + value + '\n';
       }
     } catch (err) {
@@ -44,7 +38,31 @@ exports['default'] = function (bot) {
       }
     }
 
-    message.reply('Results for survey _' + survey[id].subject + '_\n' + result);
+    return result;
+  };
+
+  var showResult = function showResult(message, close) {
+    var _message$match = _slicedToArray(message.match, 2);
+
+    var id = _message$match[1];
+
+    if (!id) return;
+
+    var survey = surveys[id];
+
+    if (close) survey.active = false;
+
+    var result = formatResult(survey);
+
+    message.reply('Results for survey _' + survey.subject + '_\n' + result);
+  };
+
+  bot.listen(/survey close (\d+)/i, function (message) {
+    showResult(message, true);
+  });
+
+  bot.listen(/survey result(?:s)? (\d+)/i, function (message) {
+    showResult(message);
   });
 
   bot.listen(/survey #(.*)# (.*)/i, function (message) {
@@ -52,6 +70,8 @@ exports['default'] = function (bot) {
 
     var subject = _message$match2[1];
     var options = _message$match2[2];
+
+    if (!subject || !options) return;
 
     options = options.split(',');
 
@@ -64,6 +84,7 @@ exports['default'] = function (bot) {
       for (var _iterator2 = options[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
         var option = _step2.value;
 
+        option = option.replace(/:/g, '');
         votes[option] = 0;
       }
     } catch (err) {
@@ -84,6 +105,7 @@ exports['default'] = function (bot) {
     var index = surveys.push({
       subject: subject, options: options, active: true, votes: votes
     }) - 1;
+    var survey = surveys[index];
 
     var im = bot.ims.find(function (im) {
       return im.user === message.user;
@@ -91,28 +113,80 @@ exports['default'] = function (bot) {
 
     bot.sendMessage(im, 'Survey id: ' + index + '\nUse this id to close the survey. See `help survey`');
 
-    message.reply(subject).then(function (r) {
-      var ts = r.ts;
-
-      bot.on('reaction_added', function listener(ev) {
-        if (ev.item.ts === ts) {
-          surveys[index].votes[ev.reaction]++;
-        }
-
-        // bot.removeListener('reaction_added', listener);
-      });
-
-      bot.on('reaction_removed', function listener(ev) {
-        if (ev.item.ts === ts) {
-          surveys[index].votes[ev.reaction]--;
-        }
-
-        // bot.removeListener('reaction_removed', listener);
-      });
+    message.reply('Survey: _' + subject + '_').then(function (r) {
+      survey.message = r;
     });
   });
 
-  bot.help('survey', 'Call for a survey', '\nsurvey #<subject># <options>\nSubject must be inside two hashes.\nOptions is a comma-separated list of acceptable reactions to\n\nsurvey close <id>\nClose the vote and show the results');
+  bot.on('reaction_added', function listener(ev) {
+    var active = surveys.filter(function (s) {
+      return s.active;
+    });
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+      for (var _iterator3 = active[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var survey = _step3.value;
+
+        if (ev.item.ts === survey.message.ts) {
+          if (typeof survey.votes[ev.reaction] === 'undefined') return;
+
+          survey.votes[ev.reaction]++;
+        }
+      }
+    } catch (err) {
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion3 && _iterator3['return']) {
+          _iterator3['return']();
+        }
+      } finally {
+        if (_didIteratorError3) {
+          throw _iteratorError3;
+        }
+      }
+    }
+  });
+
+  bot.on('reaction_removed', function listener(ev) {
+    var active = surveys.filter(function (s) {
+      return s.active;
+    });
+    var _iteratorNormalCompletion4 = true;
+    var _didIteratorError4 = false;
+    var _iteratorError4 = undefined;
+
+    try {
+      for (var _iterator4 = active[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+        var survey = _step4.value;
+
+        if (ev.item.ts === survey.message.ts) {
+          if (typeof survey.votes[ev.reaction] === 'undefined') return;
+
+          survey.votes[ev.reaction]--;
+        }
+      }
+    } catch (err) {
+      _didIteratorError4 = true;
+      _iteratorError4 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion4 && _iterator4['return']) {
+          _iterator4['return']();
+        }
+      } finally {
+        if (_didIteratorError4) {
+          throw _iteratorError4;
+        }
+      }
+    }
+  });
+
+  bot.help('survey', 'Call for a survey', '\nsurvey #<subject># <options>\nSubject must be inside two hashes.\nOptions is a comma-separated list of acceptable reaction emojis\n\nsurvey close <id>\nClose the survey and show the results\n\nsurvey result(s) <id>\nShow results of a survey');
 };
 
 module.exports = exports['default'];
--- src/index.js
+++ src/index.js
@@ -1,65 +1,95 @@
 export default bot => {
   const surveys = [];
 
-  bot.listen(/survey close (\d+)/i, message => {
-    let [, id] = message.match;
-
-    surveys[id].active = false;
-
+  const formatResult = survey => {
     let result = '';
 
-    for (let vote of Object.keys(surveys[id].votes)) {
-      let value = surveys[id].votes[vote];
+    for (let vote of Object.keys(survey.votes)) {
+      let value = survey.votes[vote];
       result += `:${vote}: => ${value}\n`;
     }
 
-    message.reply(`Results for survey _${survey[id].subject}_\n` + result);
+    return result;
+  }
+
+  const showResult = (message, close) => {
+    let [, id] = message.match;
+    if (!id) return;
+
+    let survey = surveys[id];
+
+    if (close) survey.active = false;
+
+    let result = formatResult(survey);
+
+    message.reply(`Results for survey _${survey.subject}_\n` + result);
+  }
+
+  bot.listen(/survey close (\d+)/i, message => {
+    showResult(message, true);
   });
+
+  bot.listen(/survey result(?:s)? (\d+)/i, message => {
+    showResult(message);
+  })
 
   bot.listen(/survey #(.*)# (.*)/i, message => {
     let [, subject, options] = message.match;
+    if (!subject || !options) return;
+
     options = options.split(',');
 
     let votes = {};
     for (let option of options) {
+      option = option.replace(/:/g, '');
       votes[option] = 0;
     }
 
     let index = surveys.push({
       subject, options, active: true, votes
     }) - 1;
+    let survey = surveys[index];
 
     let im = bot.ims.find(im => im.user === message.user).id;
 
     bot.sendMessage(im, `Survey id: ${index}
 Use this id to close the survey. See \`help survey\``);
 
-    message.reply(subject).then(r => {
-      let { ts } = r;
-      bot.on('reaction_added', function listener(ev) {
-        if (ev.item.ts === ts) {
-          surveys[index].votes[ev.reaction]++;
-        }
+    message.reply(`Survey: _${subject}_`).then(r => {
+      survey.message = r;
+    });
+  })
 
-        // bot.removeListener('reaction_added', listener);
-      });
+  bot.on('reaction_added', function listener(ev) {
+    let active = surveys.filter(s => s.active);
+    for (let survey of active) {
+      if (ev.item.ts === survey.message.ts) {
+        if (typeof survey.votes[ev.reaction] === 'undefined') return;
 
-      bot.on('reaction_removed', function listener(ev) {
-        if (ev.item.ts === ts) {
-          surveys[index].votes[ev.reaction]--;
-        }
-
-        // bot.removeListener('reaction_removed', listener);
-      });
-    })
+        survey.votes[ev.reaction]++;
+      }
+    }
   });
 
+  bot.on('reaction_removed', function listener(ev) {
+    let active = surveys.filter(s => s.active);
+    for (let survey of active) {
+      if (ev.item.ts === survey.message.ts) {
+        if (typeof survey.votes[ev.reaction] === 'undefined') return;
+
+        survey.votes[ev.reaction]--;
+      }
+    }
+  });
 
   bot.help('survey', 'Call for a survey', `
 survey #<subject># <options>
 Subject must be inside two hashes.
-Options is a comma-separated list of acceptable reactions to
+Options is a comma-separated list of acceptable reaction emojis
 
 survey close <id>
-Close the vote and show the results`);
+Close the survey and show the results
+
+survey result(s) <id>
+Show results of a survey`);
 }
